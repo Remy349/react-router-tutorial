a3a3a4a5a7d9b07ef946bc2853b50b06d1966c0b
Remy349 <santoraga15@gmail.com>
2023-02-13 16:54:26 -0600

    Added basic routes and error page for handling not found errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './routes/Root'
+import ErrorPage from './ErrorPage'
 
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>OK</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
